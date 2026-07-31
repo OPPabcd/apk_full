4cdc7e23a1390ce830a45d7c1b7fee2ca4082505
gajah123-cmd <158025334+OPPabcd@users.noreply.github.com>
2026-07-31 07:32:22 +0700

    reset

--- Aplikasi Absensi/android/app/src/main/kotlin/com/example/apk_full/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.apk_full
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Aplikasi Absensi/lib/dashboard/d_c_murid/function/f_Pesan/grup_sekolah_p.dart
+++
@@ -1,1 +0,0 @@
-// Deprecated. Fungsi notifikasi dipindahkan ke lib/function/f_Pesan/notif_pesan/preview_pesan.dart
